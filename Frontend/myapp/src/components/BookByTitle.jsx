d2e35b0f7a0e4a7bf7cca7097769a51e133702e4
Kashish Bansal <115978805+kashishbansal081@users.noreply.github.com>
2025-08-04 11:55:19 +0530

    Update BookByTitle.jsx
    updated the vercel url

--- Frontend/myapp/src/components/BookByTitle.jsx
+++ Frontend/myapp/src/components/BookByTitle.jsx
@@ -2,7 +2,7 @@ import useFetch from '../useFetch'
 
 const BookByTitle = ({title})=>{
     // console.log(title)
-const {data, loading, error } = useFetch(`http://localhost:3000/books/${title}`)
+const {data, loading, error } = useFetch(`https://fullstack-book-manager.vercel.app/books/${title}`)
 // console.log(data)
 
 return data ? (
