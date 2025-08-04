06ef775caed1c9f4bf573813f26223dafcd3b743
Kashish Bansal <115978805+kashishbansal081@users.noreply.github.com>
2025-08-04 23:45:50 +0530

    Update index.js
    added origin in cors

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const corsOptions = {
+  origin : "*",
   credentials: true,
   optionSuccessStatus: 200,
 };
